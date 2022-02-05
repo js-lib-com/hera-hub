924caf3b08cc8981432f83b0d93a29a21683ed46
Iulian Rotaru <mr.iulianrotaru@gmail.com>
2022-02-05 06:00:42 +0200

    Add central heating running state to thermostat dialog.

--- script/js/ui/ThermostatDialog.js
+++ script/js/ui/ThermostatDialog.js
@@ -6,65 +6,58 @@ js.ui.ThermostatDialog = function(ownerDoc, node) {
 	this.$super(ownerDoc, node);
 
 	this._panorama = ownerDoc.getByClass(js.ui.DevicesLayout);
-	
+
+	this._stateView = this.getByCss(".state");
 	this._temperatureView = this.getByCss(".value .temperature");
 	this._setpointView = this.getByCss(".value .setpoint");
 
 	this.on(this, {
-		"&close-dialog" : this._onCloseDialog,
-		"&temperature-up" : this._onTemperatureUp,
-		"&temperature-down" : this._onTemperatureDown
+		"&close-dialog": this._onCloseDialog,
+		"&temperature-up": this._onTemperatureUp,
+		"&temperature-down": this._onTemperatureDown
 	});
 };
 
 js.ui.ThermostatDialog.prototype = {
-	open : function(deviceName, callback) {
+	open: function(deviceName, callback) {
 		this._deviceName = deviceName;
 		this._callback = callback;
 		this._panorama.lock();
 
-		this._updateTemperature();
-		this._updateSetpoint();
+		Service.invokeDeviceAction(this._deviceName, "getState", [], state => {
+			this._temperatureView.setValue(state.temperature);
+			this._setpoint = state.setpoint;
+			this._setpointView.setValue(state.setpoint);
+			this._stateView.addCssClass("running", state.running);
+		});
 
 		this.show();
 	},
 
-	_onCloseDialog : function(ev) {
+	_onCloseDialog: function(ev) {
 		this.hide();
 		this._panorama.unlock();
 		this._callback();
 	},
 
-	_onTemperatureUp : function(ev) {
+	_onTemperatureUp: function(ev) {
 		this._setpoint += 0.5;
 		this._setSetpoint();
 	},
 
-	_onTemperatureDown : function(ev) {
+	_onTemperatureDown: function(ev) {
 		this._setpoint -= 0.5;
 		this._setSetpoint();
 	},
 
-	_updateTemperature : function() {
-		Service.invokeDeviceAction(this._deviceName, "getTemperature", [], function(temperature) {
-			this._temperatureView.setValue(temperature);
-		}, this);
-	},
-
-	_updateSetpoint : function() {
-		Service.invokeDeviceAction(this._deviceName, "getSetpoint", [], function(setpoint) {
+	_setSetpoint: function() {
+		Service.invokeDeviceAction(this._deviceName, "setSetpoint", [this._setpoint], setpoint => {
 			this._setpoint = setpoint;
 			this._setpointView.setValue(setpoint);
-		}, this);
+		});
 	},
 
-	_setSetpoint : function() {
-		Service.invokeDeviceAction(this._deviceName, "setSetpoint", [ this._setpoint ], function() {
-			this._updateSetpoint();
-		}, this);
-	},
-
-	toString : function() {
+	toString: function() {
 		return "js.ui.ThermostatDialog";
 	}
 };
